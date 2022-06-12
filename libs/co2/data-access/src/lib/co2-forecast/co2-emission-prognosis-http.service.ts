b2ef8dd3a6612f5ab0701b72b14ad395866e1a42
Wenlin Huang <jackhwl@hotmail.com>
2022-06-12 17:28:43 -0400

    refactor: use record collection type

--- libs/co2/data-access/src/lib/co2-forecast/co2-emission-prognosis-http.service.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-emission-prognosis-http.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable, of, throwError } from 'rxjs'
 import { mergeMap } from 'rxjs/operators'
-import { Co2EmissionPrognosisRecord } from './co2-emission-prognosis-record'
+import { Co2EmissionPrognosisRecord, Co2EmissionPrognosisRecords } from './co2-emission-prognosis-record'
 import { Co2ApiResponse } from './co2-api-response'
 import { Co2ApiErrorsResponse } from './co2-api-errors-response'
 import { energiDataServiceEndpoint } from './energi-data-service-endpoint'
@@ -14,7 +14,7 @@ import { energiDataServiceEndpoint } from './energi-data-service-endpoint'
 export class Co2EmissionPrognosisHttp {
     constructor(private http: HttpClient){}
 
-    get(): Observable<readonly Co2EmissionPrognosisRecord[]> {
+    get(): Observable<Co2EmissionPrognosisRecords> {
         return this.http.get<Co2ApiResponse<Co2EmissionPrognosisRecord> | Co2ApiErrorsResponse>(energiDataServiceEndpoint, {
             params: {
                 resource_id: 'co2emisprog',
