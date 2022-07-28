5f7a049e79f7bab784ca8c2ba8175b55bbccaa90
Wenlin Huang <jackhwl@hotmail.com>
2022-07-27 22:16:08 -0400

    feat: setup CO2 forecast container component

--- apps/wenlin-site-app/src/app/app.module.ts
+++ apps/wenlin-site-app/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent, AppScam } from './app.sfc';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, AppScam],
 })
 export class AppModule {}
--- libs/co2/feature-forecast/src/lib/co2-forecast-container.sfc.ts
+++ libs/co2/feature-forecast/src/lib/co2-forecast-container.sfc.ts
@@ -4,9 +4,9 @@ import { ChangeDetectionStrategy, Component, NgModule, ViewEncapsulation } from 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
-    selector: 'wl-forecast',
+    selector: 'wl-co2-forecast',
     styles: [`:host { display: block; }`],
-    template: ``,
+    template: `<h1>CO2 forecast</h1>`,
 })
 
 export class Co2ForecastContainerComponent {}
