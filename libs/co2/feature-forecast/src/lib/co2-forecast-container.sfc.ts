1ac4c4b95e76abe282439eac222379c5299cbb6a
Wenlin Huang <jackhwl@hotmail.com>
2022-07-29 19:54:27 -0400

    feat: display CO2 forecast

--- libs/co2/feature-forecast/src/lib/co2-forecast-container.sfc.ts
+++ libs/co2/feature-forecast/src/lib/co2-forecast-container.sfc.ts
@@ -1,28 +1,45 @@
-import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, NgModule, ViewEncapsulation } from "@angular/core";
-import { Co2ForecastStore } from "@wenlin-site/co2/data-access";
-import { Co2ForecastScam } from "./co2-forecast.sfc";
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgModule,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Co2ForecastStore } from '@wenlin-site/co2/data-access';
+import { Co2Forecast } from '@wenlin-site/co2/domain';
+import { Observable } from 'rxjs';
+import { Co2ForecastScam } from './co2-forecast.sfc';
 
 @Component({
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    encapsulation: ViewEncapsulation.None,
-    selector: 'wl-co2-forecast',
-    styles: [
-        `
-        :host { 
-            display: block; 
-        }
-        `
-    ],
-    template: `<wl-co2-forecast-ui></wl-co2-forecast-ui>`,
-    viewProviders: [Co2ForecastStore]
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  selector: 'wl-co2-forecast',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+  template: `<wl-co2-forecast-ui
+    [forecast]="forecast$ | async"
+  ></wl-co2-forecast-ui>`,
+  viewProviders: [Co2ForecastStore],
 })
+export class Co2ForecastContainerComponent {
+  #co2ForecastStore: Co2ForecastStore;
 
-export class Co2ForecastContainerComponent {}
+  forecast$: Observable<Co2Forecast>;
+
+  constructor(co2ForecasStore: Co2ForecastStore) {
+    this.#co2ForecastStore = co2ForecasStore;
+    this.forecast$ = this.#co2ForecastStore.forecast$;
+  }
+}
 
 @NgModule({
-    declarations: [Co2ForecastContainerComponent],
-    exports: [Co2ForecastContainerComponent],
-    imports: [CommonModule, Co2ForecastScam]
+  declarations: [Co2ForecastContainerComponent],
+  exports: [Co2ForecastContainerComponent],
+  imports: [CommonModule, Co2ForecastScam],
 })
 export class Co2ForecastContainerScam {}
--- libs/co2/feature-forecast/src/lib/co2-forecast.sfc.ts
+++ libs/co2/feature-forecast/src/lib/co2-forecast.sfc.ts
@@ -1,26 +1,62 @@
-import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, NgModule, ViewEncapsulation } from "@angular/core";
-import { Co2ForecastStore } from "@wenlin-site/co2/data-access";
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  NgModule,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Co2ForecastStore } from '@wenlin-site/co2/data-access';
+import { Co2Forecast } from '@wenlin-site/co2/domain';
 
 @Component({
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    encapsulation: ViewEncapsulation.None,
-    selector: 'wl-co2-forecast-ui',
-    styles: [
-        `
-        :host { 
-            display: block; 
-        }
-        `
-    ],
-    template: `<h1>CO2 forecast</h1>`,
-    viewProviders: [Co2ForecastStore]
-})
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  selector: 'wl-co2-forecast-ui',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+  template: `
+    <h1>CO2 forecast</h1>
 
-export class Co2ForecastComponent {}
+    <table>
+      <thead>
+        <tr>
+          <th>Date and time</th>
+          <th>gCO2eq/kWh</th>
+          <th>Price area</th>
+        </tr>
+      </thead>
+      <tr *ngFor="let dataPoint of forecast">
+        <td>{{ dataPoint.minutes5UTC.toJSDate() | date }}</td>
+        <td>{{ dataPoint.co2Emission | number: '1.2' }}</td>
+        <td>{{ dataPoint.priceArea }}</td>
+      </tr>
+    </table>
+  `,
+  viewProviders: [Co2ForecastStore],
+})
+export class Co2ForecastComponent {
+  static ngAcceptInputType_forecast: Co2Forecast | null;
+
+  #forecast: Co2Forecast = [];
+  @Input()
+  get forecast(): Co2Forecast {
+    return this.#forecast;
+  }
+  set forecast(forecast: Co2Forecast) {
+    forecast ??= [];
+    this.#forecast = forecast;
+  }
+}
 
 @NgModule({
-    declarations: [Co2ForecastComponent],
-    exports: [Co2ForecastComponent],
+  declarations: [Co2ForecastComponent],
+  exports: [Co2ForecastComponent],
+  imports: [CommonModule],
 })
 export class Co2ForecastScam {}
